7844d1d4b2cbb9706a822fd4caf9f0773e528d8d
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-01-08 17:34:42 +0530

    loaded model artifacts

--- api/app.py
+++ api/app.py
@@ -1,3 +1,12 @@
 from flask import Flask, jsonify
 import joblib
 from scipy.sparse import load_npz
+
+# Model artifacts
+
+MODEL_DIR = "../model"
+
+als_model = joblib.load(f"{MODEL_DIR}/als_model.pkl")
+user_item_matrix = load_npz(f"{MODEL_DIR}/user_item_matrix.npz")
+user_id_mapping = joblib.load(f"{MODEL_DIR}/user_id_mapping.pkl")
+id_to_song = joblib.load(f"{MODEL_DIR}/id_to_song.pkl")
